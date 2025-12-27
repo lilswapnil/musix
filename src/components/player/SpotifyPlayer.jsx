8e60185543691bd324dde5fcb5c42d02f8be12d1
Scott Bhalerao <lilswapnil@Scotts-MacBook-Air.local>
2025-12-27 18:11:34 -0500

    changing the color of buttons

--- src/components/player/SpotifyPlayer.jsx
+++ src/components/player/SpotifyPlayer.jsx
@@ -61,7 +61,7 @@ export default function SpotifyPlayer() {
   }
   
   return (
-    <div className="fixed bottom-0 left-0 right-0 glass border-t border-white/20 p-2 shadow-lg">
+    <div className="fixed bottom-0 left-0 right-0 glass-dark border-t border-white/20 backdrop-blur-lg p-3 shadow-lg">
       <div className="flex items-center justify-between max-w-screen-xl mx-auto">
         {/* Track info */}
         <div className="flex items-center w-1/3">
