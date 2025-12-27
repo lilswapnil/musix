4f2fa1887c1cde76b099874a2b8d97537a038fe2
Scott Bhalerao <lilswapnil@Scotts-MacBook-Air.local>
2025-12-27 13:08:26 -0500

    fixing url base link

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/musix/',
+  base: '/',
   css: {
     postcss: './postcss.config.js',
   },
